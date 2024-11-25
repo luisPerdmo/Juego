8e65f2db822352f0e5548ccba92b07261afb088b
MaicolZzz <buitragomaicol23@gmail.com>
2024-11-24 19:36:15 -0500

    Funciones agregadas en creaUsuario.py

--- View/crearUsuario.py
+++ View/crearUsuario.py
@@ -4,14 +4,25 @@ from tkinter import messagebox
 
 class CrearUsuario():
 
+    def validarCampos(self, event=None):
+    
+        nombre_valido = len(self.txtNombre.get()) >= 3
+        contrasena_valida = len(self.txtContrasena.get()) >= 6  
+
+        if nombre_valido and contrasena_valida:
+            self.btnGuardar.config(state=tk.NORMAL)
+        else:
+            self.btnGuardar.config(state=tk.DISABLED)
+
     def guardarUsuario(self, event):
         self.Usuario.crearUsuario(self.txtNombre.get(), self.txtContrasena.get())
         messagebox.showinfo("Confirmación", "¡Nuevo usuario registrado con éxito!")
-
+        self.limpiarCampos()
 
     def limpiarCampos(self):
         self.txtNombre.delete(0, END)
         self.txtContrasena.delete(0, END)
+        self.btnGuardar.config(state=tk.DISABLED)
 
     def __init__(self, Usuario):
         self.ventana = tk.Toplevel()
@@ -29,14 +40,16 @@ class CrearUsuario():
 
         self.txtNombre = tk.Entry(self.ventana)
         self.txtNombre.place(x=150, y=100, width=160, height=25)
+        self.txtNombre.bind("<KeyRelease>", self.validarCampos)
 
         self.lblContrasena = tk.Label(self.ventana, text="Contraseña*:", anchor="w")
         self.lblContrasena.place(x=50, y=150, width=80, height=25)
 
         self.txtContrasena = tk.Entry(self.ventana)
         self.txtContrasena.place(x=150, y=150, width=160, height=25)
+        self.txtContrasena.bind("<KeyRelease>", self.validarCampos)
 
-        self.btnGuardar = tk.Button(self.ventana, text="Guardar")
+        self.btnGuardar = tk.Button(self.ventana, text="Guardar", state=tk.DISABLED)
         self.btnGuardar.place(x=85, y=260, width=80, height=30)
         self.btnGuardar.bind("<Button-1>", self.guardarUsuario)
 
