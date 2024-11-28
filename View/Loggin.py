a5c3b315d6bc9fce9fb19e97bf5a2ea8a14b302e
Pedro Perdomo <pedroperdomo@ejemplo.com>
2024-11-27 21:47:01 -0500

    Arregle el loggin

--- View/Loggin.py
+++ View/Loggin.py
@@ -6,24 +6,6 @@ from View.crearUsuario import CrearUsuario
 
 class Loggin():
 
-    #Mostrar y eliminar Texto
-    def mostrarUsu(self, event):
-        if self.txtUsuario.get() == "":
-            self.txtUsuario.insert(0, "Usuario:")
-            self.txtUsuario.config(fg="gray")
-    def eliminarUsu(self, event):
-        if self.txtUsuario.get() == "Usuario:":
-            self.txtUsuario.delete(0, END)
-            self.txtUsuario.config(fg="black")
-    def mostrarPass(self, event):
-        if self.txtPassword.get() == "":
-            self.txtPassword.insert(0, "Contraseńa:")
-            self.txtPassword.config(fg="gray")
-    def eliminarPass(self, event):
-        if self.txtPassword.get() == "Contraseńa:":
-            self.txtPassword.delete(0, END)
-            self.txtPassword.config(fg="black")
-
     def validarCampos(self, event):
         if len(self.txtUsuario.get()) > 15:
             self.txtUsuario.delete(len(self.txtUsuario.get()) - 1, END)
@@ -34,27 +16,23 @@ class Loggin():
                 self.btnIngresar.config(state="normal")
             else:
                 self.btnIngresar.config(state="disabled")
-        else:
-            self.btnIngresar.config(state="disabled")
 
     def verCaracteres(self, event):
         if(self.bandera == True):
             self.txtPassword.config(show='*')
-            self.btnVer.config(image=self.IconoVer)
+            self.btnVer.config(image=self.IconoOcul)
             self.bandera = False
         else:
             self.txtPassword.config(show='')
-            self.btnVer.config(image=self.IconoOcul)
+            self.btnVer.config(image=self.IconoVer)
             self.bandera = True
 
     def limpiarCampos(self):
         self.txtUsuario.delete(0, END)
         self.txtPassword.delete(0, END)
-        self.txtUsuario.insert(0, "Usuario:")
-        self.txtPassword.insert(0, "Contraseña:")
-        self.txtUsuario.config(fg="gray")
-        self.txtPassword.config(fg="gray")
-
+        self.txtUsuario.delete(0, END)
+        self.txtPassword.delete(0, END)
+      
     def ingresar(self, event):
         miUsuario = Usuario()
         miUsuario.iniciarSesion(self.txtUsuario.get(), self.txtPassword.get(), self.ventana)
@@ -83,25 +61,26 @@ class Loggin():
         self.frame = tk.Frame(self.ventana, width=390, height=350, bg="#F0F0F0")
         self.frame.place(relx=0.5, rely=0.5, anchor="center")
 
+        #Texto
         self.lblIcono = tk.Label(self.frame, image=self.IconoLoggin, bg="#F0F0F0")
         self.lblIcono.place(x=30, y=0.1)
 
         self.lblTitulo = tk.Label(self.frame, text="Happy Cóndor", font=("Comic Sans MS", 28, "bold"), bg="#F0F0F0")
         self.lblTitulo.place(x=125, y=40)
 
+        self.lblUsuario = tk.Label(self.frame, text="Usuario*:", bg="#F0F0F0")
+        self.lblUsuario.place(x=85, y=120)
+
+        self.lblPassword = tk.Label(self.frame, text="Password*:", bg="#F0F0F0")
+        self.lblPassword.place(x=85, y=170)
+
         #Campo de entrada
         self.txtUsuario = tk.Entry(self.frame, width=25)
         self.txtUsuario.place(x=80, y=140)
-        self.txtUsuario.insert(0, "Usuario:")
-        self.txtUsuario.bind("<FocusOut>", self.mostrarUsu)
-        self.txtUsuario.bind("<FocusIn>", self.eliminarUsu)
         self.txtUsuario.bind("<KeyRelease>", self.validarCampos)
 
-        self.txtPassword = tk.Entry(self.frame, width=25)
+        self.txtPassword = tk.Entry(self.frame, width=25, show="*")
         self.txtPassword.place(x=80, y=190)
-        self.txtPassword.insert(0, "Contraseńa:")
-        self.txtPassword.bind("<FocusOut>", self.mostrarPass)
-        self.txtPassword.bind("<FocusIn>", self.eliminarPass)
         self.txtPassword.bind("<KeyRelease>", self.validarCampos)
 
         #Botones
