ec23cacd099cf3e06b011956d78ef797ba1019fc
MaicolZzz <buitragomaicol23@gmail.com>
2024-11-28 18:09:08 -0500

    Consulta sql corregida

--- Controller/Usuario.py
+++ Controller/Usuario.py
@@ -40,7 +40,7 @@ class Usuario():
         miConexion.crearConexion()
         conexion = miConexion.getConection()
         cursor = conexion.cursor()
-        cursor.execute("SELECT * FROM jugadores WHERE nombre = %s", (nombreUsu,))
+        cursor.execute("SELECT * FROM jugadores WHERE nombre = ?", (nombreUsu,))
         conexion.commit()
         resultado = cursor.fetchone()
         miConexion.cerrarConexion
